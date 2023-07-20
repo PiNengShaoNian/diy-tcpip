ab88726d9c9587000d4a82bbe00fbd02509b17db
quguiyou <quguiyou1076@qq.com>
2023-07-20 16:13:47 +0800

    接受数据包修改并发回

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -10,18 +10,26 @@ int main(void) {
   while (pcap) {
     static uint8_t buffer[1024];
     static int counter = 0;
-
+    struct pcap_pkthdr *pkthdr;
+    const uint8_t *pkt_data;
     plat_printf("begin test: %d\n", counter++);
     for (int i = 0; i < sizeof(buffer); i++) {
       buffer[i] = i;
     }
 
-    if (pcap_inject(pcap, buffer, sizeof(buffer)) == -1) {
+    if (pcap_next_ex(pcap, &pkthdr, &pkt_data) != 1) {
+      continue;
+    }
+
+    int len = pkthdr->len > sizeof(buffer) ? sizeof(buffer) : pkthdr->len;
+    plat_memcpy(buffer, pkt_data, len);
+    buffer[0] = 1;
+    buffer[1] = 2;
+
+    if (pcap_inject(pcap, buffer, len) == -1) {
       plat_printf("pcap send: send packet failed %s\n", pcap_geterr(pcap));
       break;
     }
-
-    sys_sleep(10);
   }
 
   printf("Hello, world");
