14ef38a60160030932db1c336d6cba3cb2f7fab1
quguiyou <quguiyou1076@qq.com>
2023-07-25 14:49:05 +0800

    插入定时器（升序方法）

--- src/net/src/timer.c
+++ src/net/src/timer.c
@@ -17,8 +17,9 @@ void display_timer_list(void) {
     plat_printf("%d: %s, period=%d, curr: %d ms, reload: %d ms\n", index++,
                 timer->name, (timer->flags & NET_TIMER_RELOAD) ? 1 : 0,
                 timer->curr, timer->reload);
-    plat_printf("------------- timer list end ------------\n");
   }
+
+  plat_printf("------------- timer list end ------------\n");
 }
 #else
 #define display_timer_list()
@@ -31,6 +32,34 @@ net_err_t net_timer_init(void) {
 
   dbg_info(DBG_TIMER, "done");
   return NET_ERR_OK;
+}
+
+static void insert_timer(net_timer_t *insert) {
+  nlist_node_t *node;
+
+  nlist_for_each(node, &timer_list) {
+    net_timer_t *curr = nlist_entry(node, net_timer_t, node);
+
+    if (insert->curr > curr->curr) {
+      insert->curr -= curr->curr;
+    } else if (insert->curr == curr->curr) {
+      insert->curr = 0;
+      nlist_insert_after(&timer_list, node, &insert->node);
+      return;
+    } else {
+      curr->curr -= insert->curr;
+
+      nlist_node_t *pre = nlist_node_pre(&curr->node);
+      if (pre) {
+        nlist_insert_after(&timer_list, pre, &insert->node);
+      } else {
+        nlist_insert_first(&timer_list, &insert->node);
+      }
+      return;
+    }
+  }
+
+  nlist_insert_last(&timer_list, &insert->node);
 }
 
 net_err_t net_timer_add(net_timer_t *timer, const char *name, timer_proc_t proc,
@@ -46,7 +75,7 @@ net_err_t net_timer_add(net_timer_t *timer, const char *name, timer_proc_t proc,
   timer->arg = arg;
   timer->flags = flags;
 
-  nlist_insert_last(&timer_list, &timer->node);
+  insert_timer(timer);
 
   display_timer_list();
   return NET_ERR_OK;
