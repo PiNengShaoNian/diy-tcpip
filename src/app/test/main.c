7ba94a07d67485d4ca2633c75846270b0d237629
quguiyou <quguiyou1076@qq.com>
2023-07-25 12:15:56 +0800

    增加定时器测试代码

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -9,6 +9,7 @@
 #include "nlist.h"
 #include "pktbuf.h"
 #include "sys_plat.h"
+#include "timer.h"
 #include "tools.h"
 
 static sys_sem_t sem;
@@ -310,6 +311,35 @@ void pktbuf_test(void) {
   pktbuf_free(buf);
 }
 
+void timer0_proc(struct _net_timer_t *timer, void *arg) {
+  static int count = 1;
+  plat_printf("this is %s: %d", timer->name, count++);
+}
+
+void timer1_proc(struct _net_timer_t *timer, void *arg) {
+  static int count = 1;
+  plat_printf("this is %s: %d", timer->name, count++);
+}
+
+void timer2_proc(struct _net_timer_t *timer, void *arg) {
+  static int count = 1;
+  plat_printf("this is %s: %d", timer->name, count++);
+}
+
+void timer3_proc(struct _net_timer_t *timer, void *arg) {
+  static int count = 1;
+  plat_printf("this is %s: %d", timer->name, count++);
+}
+
+void timer_test(void) {
+  static net_timer_t t0, t1, t2, t3;
+
+  net_timer_add(&t0, "t0", timer0_proc, (void *)0, 200, 0);
+  net_timer_add(&t1, "t1", timer1_proc, (void *)0, 1000, NET_TIMER_RELOAD);
+  net_timer_add(&t2, "t2", timer2_proc, (void *)0, 1000, 0);
+  net_timer_add(&t3, "t3", timer3_proc, (void *)0, 4000, 0);
+}
+
 void basic_test(void) {
   nlist_test();
   mblock_test();
@@ -317,6 +347,8 @@ void basic_test(void) {
 
   uint32_t v1 = x_ntohl(0x12345678);
   uint32_t v2 = x_ntohs(0x1234);
+
+  timer_test();
 }
 
 #define DBG_TEST DBG_LEVEL_INFO
--- src/net/src/timer.c
+++ src/net/src/timer.c
@@ -12,3 +12,8 @@ net_err_t net_timer_init(void) {
   dbg_info(DBG_TIMER, "done");
   return NET_ERR_OK;
 }
+
+net_err_t net_timer_add(net_timer_t *timer, const char *name, timer_proc_t proc,
+                        void *arg, int ms, int flags) {
+  return NET_ERR_OK;
+}
