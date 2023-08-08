aa7776fc4900c5579bc2ce5218cb10705b00fce3
quguiyou <quguiyou1076@qq.com>
2023-08-08 14:29:56 +0800

    将数据包中的数据写入缓存

--- src/net/src/tcp_in.c
+++ src/net/src/tcp_in.c
@@ -81,14 +81,38 @@ net_err_t tcp_in(pktbuf_t *buf, ipaddr_t *src_ip, ipaddr_t *dest_ip) {
     return NET_ERR_OK;
   }
 
+  net_err_t err = pktbuf_seek(buf, tcp_hdr_size(tcp_hdr));
+  if (err < 0) {
+    dbg_error(DBG_TCP, "seek failed.");
+    return NET_ERR_SIZE;
+  }
   tcp_state_proc[tcp->state](tcp, &seg);
   tcp_show_info("after tcp in", tcp);
 
   return NET_ERR_OK;
 }
 
+static int copy_data_to_rcvbuf(tcp_t *tcp, tcp_seg_t *seg) {
+  int doff = seg->seq - tcp->rcv.nxt;
+  if (seg->data_len) {
+    return tcp_buf_write_rcv(&tcp->rcv.buf, doff, seg->buf, seg->data_len);
+  }
+  return 0;
+}
+
 net_err_t tcp_data_in(tcp_t *tcp, tcp_seg_t *seg) {
+  int size = copy_data_to_rcvbuf(tcp, seg);
+  if (size < 0) {
+    dbg_error(DBG_TCP, "copy data to rcvbuf failed.");
+    return NET_ERR_SIZE;
+  }
+
   int wakeup = 0;
+  if (size) {
+    tcp->rcv.nxt += size;
+    wakeup++;
+  }
+
   tcp_hdr_t *tcp_hdr = seg->hdr;
   if (tcp_hdr->f_fin) {
     tcp->rcv.nxt++;
